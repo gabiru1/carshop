8c018bd25b1e92ba37a196f1464f5357fe3e327d
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-04-30 18:48:04 -0300

    to fix

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -3,16 +3,18 @@ import 'mocha';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { Car } from '../../../interfaces/CarInterface';
+import CarModel from "../../../models/CarModel";
 import CarService from '../../../services/CarService';
 
 describe('Testa camada Service de Car', () => {
   const carService = new CarService();
+  const carModel = new CarModel();
 
   before(() => {
-    sinon.stub(carService.model, 'create').resolves(documentMock);
+    sinon.stub(carModel.model, 'create').resolves(documentMock);
   });
 
-  after(() => (carService.model.create as sinon.SinonStub).restore());
+  after(() => (carModel.model.create as sinon.SinonStub).restore());
 
   it('Testa se um carro é criado com sucesso', async () => {
     const response = await carService.create(mokedCar as Car);
